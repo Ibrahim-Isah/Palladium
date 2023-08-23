4155126076cc79f66fd352fecc73b684c42affc3
Isah Ibrahim <isahibn08@gmail.com>
2023-08-23 15:30:09 +0100

    adding some styles

--- src/views/Product.tsx
+++ src/views/Product.tsx
@@ -2,22 +2,48 @@
 import React from "react"
 import { useAppSelector } from "../app/hooks"
 import { selectedProduct } from "../features/productSlice"
+import { Link } from "react-router-dom"
+import { onTestFailed } from "vitest"
 
 const Product = () => {
   const product = useAppSelector(selectedProduct)
 
-  console.log(product, "here is the product")
   return (
     <div>
-      <h1
+      <div
         style={{
-          fontSize: "28px",
-          fontWeight: "bold",
-          textAlign: "center",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
         }}
       >
-        Product Details Page
-      </h1>
+        <h1
+          style={{
+            fontSize: "28px",
+            fontWeight: "bold",
+            textAlign: "center",
+            flex: 1,
+          }}
+        >
+          Product Details Page
+        </h1>
+        <Link
+          to="/"
+          style={{
+            padding: "10px",
+            backgroundColor: "blue",
+            color: "white",
+            borderRadius: "5px",
+            cursor: "pointer",
+            textAlign: "center",
+            marginTop: "20px",
+            textDecoration: "none",
+          }}
+        >
+          {" "}
+          Product List
+        </Link>
+      </div>
       <div>
         <h1>{product.name}</h1>
         <p>{product.type}</p>
